26b4982138f0168895ca3159e4cdd2740035eefe
Ilia Sidorenko <ilia.sidorenko@gmail.com>
2023-05-07 10:22:23 +0300

    init commit

---
+++ BaseCode/Base.cs
@@ -0,0 +1,89 @@
+﻿using AventStack.ExtentReports;
+using AventStack.ExtentReports.Model;
+using AventStack.ExtentReports.Reporter;
+//using Microsoft.VisualStudio.TestTools.UnitTesting;
+using NUnit.Framework;
+using NUnit.Framework.Internal;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Appium.Windows;
+using OpenQA.Selenium.Chrome;
+using System;
+using System.Security.Cryptography.X509Certificates;
+
+namespace WebUITestAutomation
+{
+
+    [TestFixture]
+    //[SetUpFixture]
+    public class Base
+    {
+
+        protected WindowsDriver<WindowsElement> session;
+        protected ChromeDriver driver;
+
+        
+
+        public NUnit.Framework.TestContext TestContext;
+        public static ExtentReports extent;
+        public ExtentTest test;
+
+
+       
+
+
+        [SetUp]
+        //[TestInitialize]
+        public void StartBrowser()
+        {
+            test = extent.CreateTest(NUnit.Framework.TestContext.CurrentContext.Test.Name);
+
+        }
+
+
+
+
+        [TearDown]
+        //[TestCleanup]
+        public void AfterTest()
+        {
+
+            var status = NUnit.Framework.TestContext.CurrentContext.Result.Outcome.Status;
+            var stacktrace = NUnit.Framework.TestContext.CurrentContext.Result.StackTrace;
+
+            DateTime time = DateTime.Now;
+            String fileName = "Screenshot_" + time.ToString("hh_mm_ss") + ".png";
+
+            if (status == NUnit.Framework.Interfaces.TestStatus.Failed)
+            {
+                test.Fail("Test Failed", driver is null ? null : captureScreenshot(driver, fileName));
+                //test.Fail("Test Failed");
+                test.Log(Status.Fail, "test failed with logtrace" + stacktrace);
+            }
+            else if (status == NUnit.Framework.Interfaces.TestStatus.Passed)
+            {
+                test.Pass("Test Passed");
+            }
+
+            
+
+        }
+
+        
+
+        public MediaEntityModelProvider captureScreenshot(ChromeDriver driver, String screenShotName)
+        {
+            ITakesScreenshot ts = driver;
+            var screenshot = ts.GetScreenshot().AsBase64EncodedString;
+
+            return MediaEntityBuilder.CreateScreenCaptureFromBase64String(screenshot, screenShotName).Build();
+
+        }
+
+
+
+
+
+
+
+    }
+}
---
+++ BaseCode/Setup.cs
@@ -0,0 +1,51 @@
+﻿using AventStack.ExtentReports.Reporter;
+using AventStack.ExtentReports;
+using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using WebUITestAutomation;
+
+namespace WebUITestAutomation
+{
+    [SetUpFixture]
+    public class Setup
+    {
+        string BaseDirectory_Path = AppDomain.CurrentDomain.BaseDirectory;
+
+        [OneTimeSetUp]
+        //[ClassInitialize]
+        public void SetUp()
+        {
+
+            //string workingDirectory = Environment.CurrentDirectory;
+            //string projectDirectory = Directory.GetParent(workingDirectory).Parent.Parent.FullName;
+            //String reportPath = projectDirectory + "\\index.html";
+            String reportPath = BaseDirectory_Path; //+ "\\TestReport.html";
+            //String reportPath = BaseDirectory_Path + "\\TestReport.html";
+            var htmlReporter = new ExtentHtmlReporter(reportPath);
+            Base.extent = new ExtentReports();
+            Base.extent.AttachReporter(htmlReporter);
+            Base.extent.AddSystemInfo("Host name", "Local host");
+            Base.extent.AddSystemInfo("Environment", "QA");
+            Base.extent.AddSystemInfo("User name", "Vafa Abadi");
+
+
+
+        }
+
+        [OneTimeTearDown]
+        //[TestCleanup]
+        public void AfterAllTests()
+        {
+
+
+            Base.extent.Flush();
+
+
+
+        }
+    }
+}
